4b1bfc6e365d1db6a1f6a1acfa08ca323fd08e18
Elvis Gilbert Batista Pimentel <44851784+Kakuhiry@users.noreply.github.com>
2021-02-11 16:10:41 -0400

    Sign in and Navigator Added

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
+import LoginScreen from './src/views/LogInScreen';
+import ProfileScreen from './src/views/ProfileScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const MainNavigator = createSwitchNavigator({
+  Login: { screen: LoginScreen },
+  Profile: { screen: ProfileScreen },
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
---
+++ src/views/LogInScreen.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import {
+  Image,
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+import * as Google from "expo-google-app-auth";
+
+const IOS_CLIENT_ID =
+  "951963137071-kt6dadc0tnj01q2i1ov686l2n32tljhf.apps.googleusercontent.com";
+const ANDROID_CLIENT_ID =
+  "951963137071-i39cq9tc88r9gvom3bebsfkv4606nfr0.apps.googleusercontent.com";
+
+export default class LoginScreen extends Component {
+  signInWithGoogle = async () => {
+    try {
+      const result = await Google.logInAsync({
+        iosClientId: IOS_CLIENT_ID,
+        androidClientId: ANDROID_CLIENT_ID,
+        scopes: ["profile", "email"],
+      });
+
+      if (result.type === "success") {
+        console.log("LoginScreen.js.js 21 | ", result.user.givenName);
+        this.props.navigation.navigate("Profile", {
+          username: result.user.givenName,
+        }); //after Google login redirect to Profile
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      console.log("LoginScreen.js.js 30 | Error with login", e);
+      return { error: true };
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.tinyLogo}
+          source={require("../../assets/LogoFieldFind.png")}
+        />
+        <TouchableOpacity onPress={this.signInWithGoogle}>
+          <Image
+            style={styles.googleLogo}
+            source={require("../../assets/GoogleLogin.png")}
+          />
+        </TouchableOpacity>
+        <Text> This is cool af, yeahhsss</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#3a4750",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tinyLogo: {
+    bottom: 80,
+    width: 244,
+    height: 398,
+  },
+  googleLogo: {
+    width: 295,
+    height: 120,
+  },
+});
