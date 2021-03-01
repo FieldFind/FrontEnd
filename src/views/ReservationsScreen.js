f846ba6f21d13240f98311c84f020a7acb5fd0b7
simulated_reality <lizardogeronimo24@gmail.com>
2021-02-28 21:25:47 -0400

    ReservationScreen refactored

--- src/views/ReservationsScreen.js
+++ src/views/ReservationsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -10,12 +10,12 @@ const styles = StyleSheet.create({
     },
 });
 
-const ReservationsScreen = () => {
-  return (
-    <View style={styles.container} >
-        <Text>Reservations Screen</Text>
-    </View>
-  );
+class ReservationsScreen extends Component{
+  render() {
+    return (
+      <Text>ReservationsScreen</Text>
+    );
+  }
 }
 
 export default ReservationsScreen;
