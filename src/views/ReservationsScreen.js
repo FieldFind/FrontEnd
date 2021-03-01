d7ce954b5e691bfeefd4e9220e7133957619ee9c
simulated_reality <lizardogeronimo24@gmail.com>
2021-03-01 19:04:30 -0400

    fetching data from api

--- src/views/ReservationsScreen.js
+++ src/views/ReservationsScreen.js
@@ -1,52 +1,45 @@
 import React, {Component} from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
-
-const DATA = [
-{
-  key:"0",
-  image:"https://reactnative.dev/img/tiny_logo.png",
-  description:"Test image",
-}
-];
-
-const Item = ({ description,image}) => (
-  <View>
-    <Image source={{uri:image}} styles={{height:120, width:295, resizeMode:'stretch'}} />
-    <Text style={styles.title}>{description}</Text>
-  </View>
-);
-
-const renderItem = ({ item }) => (
-  <Item description={item.description,item.image} />
-);
+import { View, FlatList, StyleSheet, Text, StatusBar, ActivityIndicator} from 'react-native';
 
 class ReservationsScreen extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://fieldfind-backend.herokuapp.com/reservas/')
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ data: json });
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
   render() {
+    const { data, isLoading } = this.state;
+
     return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-      />
-    </SafeAreaView>
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Text>{item.espacio.nombre_espacio}</Text>
+            )}
+          />
+        )}
+      </View>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: '#CDD7D6',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 20,
-  },
-});
+};
 
 export default ReservationsScreen;
