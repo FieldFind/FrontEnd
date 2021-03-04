3907224033e0af2fe5ba1fe8d0368d42a36399c6
simulated_reality <lizardogeronimo24@gmail.com>
2021-03-04 00:29:43 -0400

    tab navigation refactor

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,15 +2,14 @@ import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from '../views/LogInScreen';
 import MainScreen from '../views/MainScreen';
 import ProfileScreen from '../views/ProfileScreen';
-import ReservationsScreen from '../views/ReservationsScreen';
+//import ReservationsScreen from '../views/ReservationsScreen';
 import DetailReservation from '../views/DetailReservationScreen';
-
 
 const MainNavigator = createSwitchNavigator({
   Login: { screen: LoginScreen },
   Profile: { screen: ProfileScreen },
   Main: {screen: MainScreen},
-  Reservations: {screen: ReservationsScreen},
+  //Reservations: {screen: ReservationsScreen},
   DetailReservation: {screen: DetailReservation}
 });
 
